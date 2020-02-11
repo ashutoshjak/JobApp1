08636ba4d24eef9f8c164a4ec546fdc67077950f
ashutoshjak <58728879+ashutoshjak@users.noreply.github.com>
2020-02-11 21:58:48 +0545

    Job App

---
+++ app/src/main/java/com/example/jobapp/setting.kt
@@ -0,0 +1,12 @@
+package com.example.jobapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class setting : MainActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
